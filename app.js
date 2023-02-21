60984f812e3d999b1e79468a0d544c52241720cb
AnnaKarasa <115081822+AnnaKarasa@users.noreply.github.com>
2023-02-21 07:46:11 +0200

    Add files via upload
    Updated API, added weather icon

--- app.js
+++ app.js
@@ -18,27 +18,33 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return "${day} ${hours}:${minutes}";
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
+  let iconElement = document.querySelector("icon");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0].icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "6f894392648bad012tbdfo34b19f8650";
 let city = "Paris";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?city=q${city}}&key=6f894392648bad012tbdfo34b19f8650&units=metric";
+let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
